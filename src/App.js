9f8339464b825cc20ba6094ffa06e959e354c690
Joey <joey7040@hotmail.com>
2020-12-20 13:34:08 -0500

    completed header css and began on sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Header />
+
+      <div className="app__body">
+        <Sidebar />
+        {/* sidebar */}
+        {/* feed */}
+        {/* widgets */}
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/Sidebar.css';
+
+
+function Sidebar() {
+    return (
+        <div classname='sidebar'>
+            
+        </div>
+    )
+}
+
+export default Sidebar
