727b12299626d0a0408fc7e071ea5e1f69018850
Joey <joey7040@hotmail.com>
2021-01-01 14:54:43 -0500

    new messageSender component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import StoryReel from './StoryReel';
+import MessageSender from './MessageSender';
 import '../css/Feed.css';
 
 function Feed() {
--- src/components/MessageSender.js
+++ src/components/MessageSender.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {Avatar} from "@material-ui/core";
 
 function MessageSender() {
     return (
         <div className="messageSender">
-            
+            <div className="messageSender__top">
+                <Avatar />
+                <form>
+                    <input className="messageSender__input" placeholder={'Whats on your mind'}/>
+                    <input placeholder="image url (optional)"></input>
+                </form>
+            </div>
         </div>
     )
 }
